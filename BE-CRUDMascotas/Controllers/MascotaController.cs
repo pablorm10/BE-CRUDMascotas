8c43d600e5c7d493835c193a776f752c6522f21d
pablorm10 <109965814+pablorm10@users.noreply.github.com>
2023-01-24 13:43:12 +0100

    Add files via upload

---
+++ BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,128 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly AplicationDbContext _context;
+
+        public MascotaController(AplicationDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listMascotas);
+
+            }catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            try { 
+               var mascota = await _context.Mascotas.FindAsync(id);
+
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+                return Ok(mascota);
+            }
+            catch(Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try {
+                var mascota = await _context.Mascotas.FindAsync(id);
+
+                if (mascota == null)
+                {
+                    return NotFound();
+                }
+
+                _context.Remove(mascota);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(Mascota mascota)
+        {
+            try
+            {
+                mascota.FechaCreacion = DateTime.Now;
+                _context.Add(mascota);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("Get", new { id = mascota.Id}, mascota);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Mascota mascota)
+        {
+            try
+            {
+                if(id != mascota.Id)
+                {
+                    BadRequest();
+                }
+
+                var mascotaItem = await _context.Mascotas.FindAsync(id);
+
+                if (mascotaItem == null)
+                {
+                    return NotFound();
+                }
+
+                mascotaItem.Nombre = mascota.Nombre;
+
+                mascotaItem.Edad = mascota.Edad;
+
+                mascotaItem.Peso = mascota.Peso;
+
+                mascotaItem.Color = mascota.Color;
+
+                mascotaItem.Raza= mascota.Raza;
+
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+
+}
---
+++ BE-CRUDMascotas/Program.cs
@@ -0,0 +1,49 @@
+using BE_CRUDMascotas.Models;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Cors
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "AllowWebapp",
+                      builder =>
+                      {
+                          builder.AllowAnyOrigin()
+                         .AllowAnyMethod()
+                         .AllowAnyHeader();
+                      });
+});
+
+// Ad Context
+builder.Services.AddDbContext<AplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conexion"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowWebapp");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
